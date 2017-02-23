fc49cf677c69f58a5a25517e6f18e348e1c8843c
Rene Preissel <rene.preissel@etosquare.de>
2017-02-23 15:32:51 +0100

    initial

---
+++ src/main/groovy/de/e2/docker/DockerPlugin.groovy
@@ -0,0 +1,113 @@
+package de.e2.docker
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.resources.ResourceException
+
+class DockerPlugin implements Plugin<Project> {
+    final static localExcludeFileName = 'docker.local.exclude'
+    final static templatSuffix = '.template'
+    final static renameClosure = {
+        String fileName ->
+            fileName.replace(templatSuffix, '')
+    }
+
+    void apply(Project project) {
+        project.getPluginManager().apply(JavaPlugin.class);
+
+        project.extensions.create("docker", DockerPluginExtension, project)
+        project.configurations.create('docker')
+
+        project.task('copyDockerFiles') { task ->
+            group = 'docker'
+            inputs.file { project.configurations.docker }
+            inputs.file { project.jar.archivePath }
+            outputs.dirs { project.docker.buildDir }
+            inputs.property 'templateMap', {
+                return createTemplateMap(project)
+            }
+
+            doLast {
+                project.copy {
+                    from project.jar.archivePath
+                    into project.extensions.docker.buildDir
+                }
+
+                def templateMap = task.inputs.properties.templateMap.clone()
+                boolean localBuild = project.docker.jenkinsBuildNumber == DockerPluginExtension.LOCAL_BUILD_NUMBER
+
+                List<String> localExcludeContent = readLocalExcludePatterns(project)
+                project.configurations.docker.forEach { file ->
+                    def srcFiles = isArchive(file) ?  project.zipTree(file) : project.fileTree(file)
+
+                    project.copy {
+                        from srcFiles
+                        into project.extensions.docker.buildDir
+
+                        expand templateMap
+                        include '*' + templatSuffix
+                        if(localBuild) {
+                            exclude localExcludeContent
+                        }
+                        exclude localExcludeFileName
+                        rename renameClosure
+                    }
+
+                    project.copy {
+                        from srcFiles
+                        into project.extensions.docker.buildDir
+                        exclude '*' + templatSuffix
+                        if(localBuild) {
+                            exclude localExcludeContent
+                        }
+                        exclude localExcludeFileName
+                    }
+                }
+            }
+        }
+    }
+
+
+    private boolean isArchive(File file) {
+        file.getName().endsWith(".jar") || file.getName().endsWith(".zip")
+    }
+
+    private List<String> readLocalExcludePatterns(Project project) {
+        List localExcludeContent = project.configurations.docker.findResults { file ->
+            if (isArchive(file)) {
+                try {
+                    return project.resources.text.fromArchiveEntry(file, localExcludeFileName).asReader().readLines()
+                } catch (ResourceException exc) {
+                    return null;
+                }
+            } else if (file.getName().equals(localExcludeFileName)) {
+                return project.resources.text.fromFile(file).asReader().readLines();
+            }
+
+            return null;
+        }.flatten()
+
+        return localExcludeContent
+    }
+
+    private Map createTemplateMap(Project project) {
+        def rawProperties = convertValuesToStrings(
+                project.docker.properties.findAll { k, v ->
+                    return v instanceof Serializable
+                })
+
+        def properties = rawProperties + rawProperties.extras
+        return properties
+    }
+
+    private Map convertValuesToStrings(Map map) {
+        return map.collectEntries { k, v ->
+            if (v instanceof Map) {
+                [(k): convertValuesToStrings(v)]
+            } else {
+                [(k): v?.toString()]
+            }
+        }
+    }
+}
---
+++ src/main/groovy/de/e2/docker/DockerPluginExtension.groovy
@@ -0,0 +1,41 @@
+package de.e2.docker
+
+import org.gradle.api.Project
+
+class DockerPluginExtension {
+    public static final String LOCAL_BUILD_NUMBER = 'local'
+
+    Project project
+
+    DockerPluginExtension(Project project) {
+        this.project = project
+        this.buildDir = project.file("${project.buildDir}/docker")
+    }
+
+    File buildDir
+
+    def appPort = 8080
+    def yourkitPort = 10001
+    def appName = "${-> project.jar.baseName}"
+    def jarName = "${-> project.jar.archiveName}"
+    def maxMem = "256m"
+    def registryUrl = "localhost:5000"
+    def imageName = "${-> appName}:${-> tagVersion}"
+    def localContainerName = "${-> appName}"
+    def localAppPort = 8080
+    def localYourkitPort = 8080
+    def localPortMappings = "-p ${-> localAppPort}:${-> appPort} -p ${-> localYourkitPort}:${-> yourkitPort}"
+    def testHost ="testHost"
+    def testAppPort = "${-> appPort}"
+    def testPortMappings = "-p ${-> testAppPort}:${-> appPort} -p ${-> testAppPort.toInteger() + 1000}:${-> yourkitPort}"
+    def testContainerName = "${-> appName}-test"
+    def prodHost = "prodHost"
+    def prodAppPort = "${-> appPort}"
+    def prodPortMappings = "-p ${-> prodAppPort}:${-> appPort}  -p ${-> prodAppPort.toInteger() + 1000}:${-> yourkitPort}"
+    def prodContainerName = "${-> appName}-prod"
+    final def jenkinsBuildNumber = "${-> System.env.BUILD_NUMBER ?: LOCAL_BUILD_NUMBER}"
+    final def tagVersion = "${-> System.env.BRANCH_NAME ?: System.properties['user.name']}-${-> jenkinsBuildNumber}"
+
+
+    Map extras = [:]
+}
